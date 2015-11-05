3d7d5d8c6054ae3b2b739d7de4431cc17e770c07
Alejandro López Sánchez <a.lopez.sanchez@udc.es>
2015-11-05 20:47:16 +0100

    clase junit

--- vvs-project/src/test/java/songstest/SongTest.java
+++ vvs-project/src/test/java/songstest/SongTest.java
@@ -1,11 +1,14 @@
 package songstest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class SongTest {
 
 	@Test
-	public void pruebaTest() {
-		
+	public void test() {
+		assertTrue(true);
 	}
+
 }
