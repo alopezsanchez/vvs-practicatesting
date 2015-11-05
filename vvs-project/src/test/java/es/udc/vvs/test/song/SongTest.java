ff236e6150de3cd8d579045e7ca7a7c6cb0940ae
Alejandro López Sánchez <a.lopez.sanchez@udc.es>
2015-11-05 20:59:46 +0100

    junit

--- vvs-project/src/test/java/es/udc/vvs/test/song/SongTest.java
+++ vvs-project/src/test/java/es/udc/vvs/test/song/SongTest.java
@@ -1,14 +1,16 @@
-package songstest;
+package es.udc.vvs.test.song;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
 
 public class SongTest {
 
 	@Test
 	public void test() {
 		assertTrue(true);
+		assertEquals(1,1);
 	}
 
 }
